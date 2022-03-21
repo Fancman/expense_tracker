d16f98d81d6ba44ffc125aa717de34c0fc37c39d
Tomas Figura <tomas.figura@post.cz>
2022-03-21 11:10:22 +0100

    Installed Alpinejs and Livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Document</title>
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<script src="{{ asset('js/app.js') }}" defer></script>
+
+	@livewireStyles
 </head>
 
 <body>
@@ -29,6 +32,8 @@
             </header>
         </div>
     </div>
+	
+	@livewireScripts
 </body>
 
 </html>
