e6053ef549e516eaa9a4ecdbe05061acb0197b44
Tomas Figura <tomas.figura@post.cz>
2022-04-11 10:24:30 +0200

    Added back composer files

--- database/migrations/2022_03_20_222427_create_categories_table.php
+++ database/migrations/2022_03_20_222427_create_categories_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
 			$table->foreignIdFor(User::class);
-            $table->string('name', 50)->unique();
+            $table->string('name', 50);
             $table->string('icon', 50)->nullable();
         });
     }
