cca25a031ccac5e683246da524a41b37249f75be
Tomas Figura <tomas.figura@post.cz>
2022-04-21 00:03:37 +0200

    User id

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -20,6 +20,7 @@ class AccountController extends Controller
 	public function refresh_prices()
     {
 		$user_id = (auth()->user() ? auth()->user()->id : 4);
+		echo 'User_id: ' . $user_id;
         //UpdatePrices::dispatch($user_id)->onQueue('host-laravel-d13296:worker_0');
         UpdatePrices::dispatch($user_id);
     }
