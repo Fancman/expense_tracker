ca906f9251419397c62b69b0641cce5e7b432bc8
Tomas Figura <tomas.figura@post.cz>
2022-04-21 00:06:19 +0200

    Default user id null

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -19,7 +19,7 @@ class AccountController extends Controller
 
 	public function refresh_prices()
     {
-		$user_id = (auth()->user() ? auth()->user()->id : 4);
+		$user_id = (auth()->user() ? auth()->user()->id : null);
 		echo 'User_id: ' . $user_id;
         //UpdatePrices::dispatch($user_id)->onQueue('host-laravel-d13296:worker_0');
         UpdatePrices::dispatch($user_id);
