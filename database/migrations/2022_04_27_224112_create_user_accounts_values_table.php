bd543cc7a44f43ab1614617a3171e47afb785011
Tomas Figura <tomas.figura@post.cz>
2022-04-28 00:49:04 +0200

    Created empty commands and tables for storing values and prices

---
+++ database/migrations/2022_04_27_224112_create_user_accounts_values_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_accounts_values', function (Blueprint $table) {
+            $table->id();
+			$table->foreignIdFor(User::class);
+			$table->decimal('value', $precision = 8, $scale = 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_accounts_values');
+    }
+};
---
+++ database/migrations/2022_04_27_224423_create_stock_prices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Currency;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_prices', function (Blueprint $table) {
+            $table->id();
+			$table->string('name', 50);
+			$table->foreignIdFor(Currency::class);
+			$table->decimal('value', $precision = 8, $scale = 2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_prices');
+    }
+};
