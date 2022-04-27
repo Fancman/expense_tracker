a9f197334bc119c05b0dd8ea279b95e987bf954a
Tomas Figura <tomas.figura@post.cz>
2022-04-28 01:09:30 +0200

    Save account value command finished

--- app/Console/Commands/SaveAccountValue.php
+++ app/Console/Commands/SaveAccountValue.php
@@ -2,7 +2,10 @@
 
 namespace App\Console\Commands;
 
+use App\Models\AccountItem;
+use App\Models\AccountValues;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class SaveAccountValue extends Command
 {
@@ -11,7 +14,7 @@ class SaveAccountValue extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'command:save_account_values';
 
     /**
      * The console command description.
@@ -27,6 +30,24 @@ class SaveAccountValue extends Command
      */
     public function handle()
     {
+		$account_values = AccountItem::select(DB::raw('users.id, SUM(quantity * current_price) as total_account_value'))
+		->join('accounts', 'account_items.account_id', '=', 'accounts.id')
+		->join('users', 'accounts.user_id', '=', 'users.id')
+		->groupBy('users.id')
+		->get();
+
+		foreach($account_values as $account_value){
+			$total_account_value = $account_value->total_account_value;
+			$user_id = $account_value->id;
+
+			$account_value = new AccountValues;
+			$account_value->user_id = $user_id;
+			$account_value->value = $total_account_value;
+			$account_value->save();
+		}
+
+		
+
         return 0;
     }
 }
